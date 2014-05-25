c7de5223583551db6470637810888c36a87ce366
codingdojoln <codingdojoln@codingdojoln-VirtualBox>
2014-05-25 22:06:40 +0200

    Added test "CanAddTwoFlags" in "ArgumentClassTest"

--- CodingDojoLN_Gtest/test/ArgumentClassTest.cpp
+++ CodingDojoLN_Gtest/test/ArgumentClassTest.cpp
@@ -18,3 +18,27 @@ TEST(ArgumentClassTest, CanAddOneFlag)
 	ASSERT_EQ("d", storedFlag.getFlag());
 	ASSERT_EQ(FlagType_TEXT, storedFlag.getType());
 }
+
+TEST(ArgumentClassTest, CanAddTwoFlags)
+{
+	//Setup
+	FlagClass flag1;
+	flag1.setFlag("d", FlagType_TEXT);
+	FlagClass flag2;
+	flag2.setFlag("g", FlagType_INTEGER);
+
+	ArgumentClass argumentClass;
+
+	//Exercise
+	argumentClass.addFlag(flag1);
+	argumentClass.addFlag(flag2);
+
+	//Verify
+	std::vector<FlagClass> flags = argumentClass.getFlags();
+	FlagClass storedFlag = flags[0];
+	ASSERT_EQ("d", storedFlag.getFlag());
+	ASSERT_EQ(FlagType_TEXT, storedFlag.getType());
+	storedFlag = flags[1];
+	ASSERT_EQ("g", storedFlag.getFlag());
+	ASSERT_EQ(FlagType_INTEGER, storedFlag.getType());
+}
