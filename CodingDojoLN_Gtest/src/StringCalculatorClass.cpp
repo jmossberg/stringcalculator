79c39afb10142448eccf803375c17f9c67279e39
codingdojoln <codingdojoln@codingdojoln-VirtualBox>
2014-09-18 21:49:43 +0200

    Learn how while(ss.getline(...)) evaluates to TRUE or FALSE by creating a istream_trace class that inherits from istream class and overloads "operator void*() const" where the return reponse is printed out.

--- CodingDojoLN_Gtest/src/StringCalculatorClass.cpp
+++ CodingDojoLN_Gtest/src/StringCalculatorClass.cpp
@@ -1,10 +1,33 @@
 #include <cstdlib>
 #include <sstream>
+#include <iostream>
 #include "StringCalculatorClass.h"
+
+
+class istream_trace : public std::istream
+{
+public:
+	operator void*() const {
+		bool found_word = std::istream::operator void*();
+		//std::cout << "Getline found word: " << found_word << "\n";
+		return reinterpret_cast<void *>(found_word);
+	}
+};
+
+class stringstream_trace : public std::stringstream
+{
+public:
+	istream_trace& getline (char* s, std::streamsize n, char delim )
+	{
+		std::istream& is = std::stringstream::getline(s, n, delim);
+		return reinterpret_cast<istream_trace&>(is);
+	}
+};
+
 
 int StringCalculatorClass::add(std::string newString) {
 	int number = 0;
-	std::stringstream ss;
+	stringstream_trace ss;
 	char TEMP_BUF[50];
 
 	//Copy string to stringstream
