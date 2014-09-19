baabab68d0fb6650678753302f829febbdf1473d
codingdojoln <codingdojoln@codingdojoln-VirtualBox>
2014-09-19 23:00:45 +0200

    Removed unused code

--- CodingDojoLN_Gtest/src/StringCalculatorClass.cpp
+++ CodingDojoLN_Gtest/src/StringCalculatorClass.cpp
@@ -3,50 +3,8 @@
 #include <exception>
 #include "StringCalculatorClass.h"
 
-
-
-
-class istream_trace : public std::istream
-{
-public:
-	operator void*() const {
-		bool found_word = std::istream::operator void*();
-		//std::cout << "Getline found word: " << found_word << "\n";
-		return reinterpret_cast<void *>(found_word);
-	}
-};
-
-class stringstream_trace : public std::stringstream
-{
-public:
-	istream_trace& getline (char* s, std::streamsize n, char delim )
-	{
-		std::istream& is = std::stringstream::getline(s, n, delim);
-		return reinterpret_cast<istream_trace&>(is);
-	}
-};
-
-
-//int StringCalculatorClass::add(std::string newString) {
-//	int number = 0;
-//	stringstream_trace ss;
-//	char TEMP_BUF[50];
-//
-//	//Copy string to stringstream
-//	ss << newString;
-//
-//	const int MAX_LENGTH_OF_NUMBER = 6;
-//	const char DELIMITER = ',';
-//
-//	while(ss.getline(TEMP_BUF, MAX_LENGTH_OF_NUMBER, DELIMITER)) {
-//		number += atoi(TEMP_BUF);
-//	}
-//
-//	return number;
-//}
-
 int StringCalculatorClass::add(std::string addString) {
-	stringstream_trace ss;
+	std::stringstream ss;
 
 	//Copy string to stringstream
 	ss << addString;
@@ -58,10 +16,6 @@ int StringCalculatorClass::add(std::string addString) {
 
 	while(false == ss.eof()) {
 		ss >> number;
-//		if(number < 0) {
-//			throwException = true;
-//			exceptionMessage << number << ",";
-//		}
 
 		this->checkForNegativeNumber(number);
 		sum += number;
@@ -69,14 +23,6 @@ int StringCalculatorClass::add(std::string addString) {
 	}
 
 	this->throwIfNegativeNumbers();
-
-//	if (throwException) {
-//		//remove last comma from exception message
-//		std::string exceptionMessageStr = exceptionMessage.str();
-//		exceptionMessageStr.resize(exceptionMessageStr.size() - 1);
-//
-//		throw exceptionMessageStr;
-//	}
 
 	return sum;
 }
